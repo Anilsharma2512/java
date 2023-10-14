af53ef83e6cfd21b10cb126f4c215910867e6387
Anilsharma2512 <146522523+Anilsharma2512@users.noreply.github.com>
2023-10-14 12:45:19 +0530

    Add files via upload

---
+++ python_tuple.py
@@ -0,0 +1,11 @@
+t = (1, 2, 3, 4, 5)
+print("The value in tuple are:",t)
+t=t+(7,)
+print("Tuple after adding element :",t)
+print("Tuple after slice : ", t[2:4])
+print("Multiplication :",(t*2))
+print(" The length of tuple is:", len(t))
+print("The maximum value of tuple is: ",max(t))
+print("The maximum value of tuple is: ",min(t))
+del t
+print("Tuple deleted" )
